feb5552773acd38c89d42cd380e14b458ec484b2
Khalid Hosein <khalid.hosein9@gmail.com>
2018-06-20 09:48:43 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import NewPost from '../src/NewPost';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <NewPost />
+          </div>
+          <div className="col-md-6">
+            Display Post
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
